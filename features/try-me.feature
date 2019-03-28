f5bf01e7c0f9c2b40dc839babb66bdfe4148c9f8
Guillaume Delré <delre.guillaume@gmail.com>
2019-03-28 16:41:03 +0100

    try to test coverage with behat

--- features/try-me.feature
+++ features/try-me.feature
@@ -1,8 +1,13 @@
 Feature:
   In order to prove that the Behat Symfony extension is correctly installed
   As a user
-  I want to have a demo scenario
+  I want to have a try-me scenario
 
   Scenario: It receives a response from Symfony's kernel
     When a demo scenario sends a request to "/"
     Then the response should be received
+
+  Scenario: It receives a response from Symfony's kernel
+    When I am on "/default"
+    Then the response should be in JSON
+    And print last JSON response
---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\MyService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/default", name="default")
+     *
+     * @param MyService $myService
+     *
+     * @return JsonResponse
+     */
+    public function index(MyService $myService)
+    {
+        $myService->setDataBy('test', ['property' => 'value']);
+        return $this->json([
+            'message' => $myService->getDataByKey('test'),
+            'path' => 'src/Controller/DefaultController.php',
+        ]);
+    }
+}
